e7f4466409b436f69e9bbd7fd6e42c458742d110
carlosstenzel <carlosstenzel@hotmail.com>
2020-01-31 18:49:06 -0300

    adicionando ao carrinho

--- loja/src/store/modules/cart/reducer.js
+++ loja/src/store/modules/cart/reducer.js
@@ -1,3 +1,14 @@
-export default function cart() {
-  return [];
+export default function cart(state, action) {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    default:
+      if(!state){
+        return [];
+      }
+      else{
+        return state;
+      }
+
+  }
 }
